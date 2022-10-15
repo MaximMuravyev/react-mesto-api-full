f9fdfc9a45b71e0088c556de4a04a4b45a3f788c
Maxim Muravyev <y89108850235@yandex.ru>
2022-10-15 19:11:33 +0300

    PW15

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,31 @@ const errorHandler = require('./middlewares/error-handler');
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+const allowedCors = [
+  'https://artyom.trus.nomoredomains.icu',
+  'http://artyom.trus.nomoredomains.icu',
+];
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.use('/', authRouter);
 app.use(cookieParser());

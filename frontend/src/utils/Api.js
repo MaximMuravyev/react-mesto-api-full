597d67b8cffc40b4207cbf426823dc5b4cab8fb0
Maxim Muravyev <y89108850235@yandex.ru>
2022-10-17 03:02:55 +0300

    PW15

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -65,8 +65,8 @@ class Api {
       headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`,
         "Content-Type": "application/json",
-        body: JSON.stringify(data),
       },
+      body: JSON.stringify(data),
     }).then(this._errorHandler);
   }
 
@@ -83,9 +83,9 @@ class Api {
       headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`,
         "Content-Type": "application/json",
-        body: JSON.stringify(data),
       },
-    }).then(this._getResponseData);
+      body: JSON.stringify(data),
+    }).then(this._errorHandler);
   }
 }
 
